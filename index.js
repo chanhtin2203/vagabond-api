0ebaf2854d01885089000b2ca4d70c4c1d788bb0
chanhtin2203 <56529818+chanhtin2203@users.noreply.github.com>
2022-11-09 16:34:41 +0700

    1

--- index.js
+++ index.js
@@ -26,25 +26,25 @@ mongoose.connect(process.env.MONGODB_URL, () => {
 app.use(
   cors({
     credentials: true,
-    origin: "https://vagabond-kappa.vercel.app",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     optionsSuccessStatus: 200,
-    allowedHeaders: [
-      //   "Access-Control-Allow-Headers",
-      //   "Access-Control-Request-Headers",
-      "Access-Control-Allow-Origin",
-      //   "Access-Control-Allow-Methods",
-      //   "Origin",
-      //   "WithCredentials",
-      //   "X-Requested-With",
-      //   "Content-Type",
-      //   "Accept",
-      //   "Authorization",
-      //   "X-HTTP-Method-Override",
-      //   "Cookie",
-      //   "Set-Cookie",
-      //   "Request",
-    ],
+    // allowedHeaders: [
+    //   "Access-Control-Allow-Headers",
+    //   "Access-Control-Request-Headers",
+    // "Access-Control-Allow-Origin",
+    //   "Access-Control-Allow-Methods",
+    //   "Origin",
+    //   "WithCredentials",
+    //   "X-Requested-With",
+    //   "Content-Type",
+    //   "Accept",
+    //   "Authorization",
+    //   "X-HTTP-Method-Override",
+    //   "Cookie",
+    //   "Set-Cookie",
+    //   "Request",
+    // ],
   })
 );
 app.use(cookieParser());
